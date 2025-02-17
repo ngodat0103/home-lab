8e7815e69151882e5563f8c65e005ec6cbcebf4d
ngodat0103 <ngovuminhdat@gmail.com>
2025-02-17 15:00:51 +0700

    config(tf,dns): add gitlab and bitwarden

---
+++ tf/cloudflare/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+locals {
+  cloudflare_api_token = var.cloudflare_api_token
+  ddns_content = var.ddns_content
+  zone_id = "0649c534ced1cb43ddc06d6a978b1dec"
+  share_comment = "Managed by Terraform"
+}
+
+provider "cloudflare" {
+  api_token = local.cloudflare_api_token
+}
+
+resource "cloudflare_dns_record" "gitlab_dnsRecord" {
+  zone_id = local.zone_id
+  comment = local.share_comment
+  content = local.ddns_content
+  name = "gitlab"
+  proxied = true
+  ttl = 1
+  type = "CNAME"
+}
+
+resource "cloudflare_dns_record" "bitwarden_dnsRecord"{
+  zone_id = local.zone_id
+  comment = local.share_comment
+  content = local.ddns_content
+  name = "bitwarden"
+  proxied = true
+  ttl = 1
+  type = "CNAME"
+}
---
+++ tf/cloudflare/vars.tf
@@ -0,0 +1,10 @@
+variable "cloudflare_api_token" {
+  type = string
+  description = "The Cloudflare API token"
+  sensitive = true
+}
+variable "ddns_content" {
+  type = string
+  description = "The content of the DDNS record"
+  sensitive = true
+}
