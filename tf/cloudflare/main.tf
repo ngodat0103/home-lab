97396f69a084012d83f4ab37f6038ec99f0a1acc
ngodat0103 <ngovuminhdat@gmail.com>
2025-05-25 20:08:33 +0700

    config(tf,cloudflare,firewall): Add block all except ip from VN

--- tf/cloudflare/main.tf
+++ tf/cloudflare/main.tf
@@ -12,6 +12,7 @@ locals {
   ddns_content = var.ddns_content
   zone_id = "0649c534ced1cb43ddc06d6a978b1dec"
   share_comment = "Managed by Terraform"
+  account_id = "4c8ad4e9fa8213af3fd284bb97b68b5e"
 }
 
 provider "cloudflare" {
@@ -46,4 +47,25 @@ resource "cloudflare_dns_record" "duc_spring_dnsRecord"{
   proxied = false
   ttl = 1
   type = "CNAME"
+}
+
+
+
+## Firewall
+
+
+# Need to import this resource from managed by cloudflare first
+## Example: 
+## terraform import cloudflare_ruleset.default zones/0649c534ced1cb43ddc06d6a978b1dec/6580faae48cc4419bbd3fde67dc58d40
+resource "cloudflare_ruleset" "default" {
+  kind = "zone"
+  name = "default"
+  phase = "http_request_firewall_custom"
+  zone_id = local.zone_id
+  rules = [{
+    action = "block"
+    description = "Block when the SOURCE IP address is not in Vietnam"
+    enabled = true
+    expression = "(ip.src.country ne \"VN\")"
+  }]
 }
