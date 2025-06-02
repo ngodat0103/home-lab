bb0ba730821a8bbbdf37245238a7f20e6087f631
ngodat0103 <ngovuminhdat@gmail.com>
2025-06-02 08:42:56 +0700

    config(): Sync backup to aerith-google-drive

--- ansible/docker/services/files/gitlab-backup.sh
+++ ansible/docker/services/files/gitlab-backup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /mnt/data2/gitlab
 docker exec gitlab gitlab-backup create
-mkdir -p /mnt/cloud/gitlab
-rsync -aP backups/ /mnt/cloud/gitlab/
--- ansible/docker/services/files/vaultwarden-backup.sh
+++ ansible/docker/services/files/vaultwarden-backup.sh
@@ -3,4 +3,4 @@ cd ~
 pg_dump > data-backup.sql
 tar -cf vaultwarden-backup-latest.tar .
 mv vaultwarden-backup-latest.tar /mnt/cloud/
-chmod 600 /mnt/cloud/vaultwarden-backup-latest.tar
+chmod 600 /mnt/recovery/cloud/vaultwarden-backup-latest.tar
