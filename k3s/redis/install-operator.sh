591269f113c9f7601d3dd0e63559755cb513eb52
ngodat0103 <ngovuminhdat@gmail.com>
2025-07-17 10:39:51 +0700

    delete directory

--- k3s/redis/install-operator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-helm repo add ot-helm https://ot-container-kit.github.io/helm-charts/
-helm upgrade redis-operator ot-helm/redis-operator \
-  --install --create-namespace --namespace redis --create-namespace
