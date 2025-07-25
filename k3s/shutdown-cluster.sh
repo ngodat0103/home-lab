c2858738b9920899819ec42447f371c8a41949a3
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2025-07-25 17:48:29 +0700

    optimize(script): optimize, use centralize repo for host common script

--- k3s/shutdown-cluster.sh
+++ k3s/shutdown-cluster.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+source <(curl -fsSL https://raw.githubusercontent.com/ngodat0103/common-stuff/refs/heads/main/0-shell/7-log/log.sh)
 # pre-shutdown task, make sure to scaled pod having LongHorn volume and make sure volume is detach
-./task-before-shutdown-longhorn.sh
-vagrant halt
+
+source <(curl -fsSL https://raw.githubusercontent.com/ngodat0103/common-stuff/refs/heads/main/0-shell/8-longhorn/pre-shutdown-longhorn.sh)
+pre-shutdown-longhorn
+
+vagrant halt && print_info "All machined all stoped succesfully" || print_error "Stop instances failed:"
