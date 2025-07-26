9fb177226768ad6b9d5eb62148e5da7b44cfe8da
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2025-07-26 18:24:00 +0700

    config(sealed-secre ): Update NodeAffinity to prefer cloud-uit: true

---
+++ k3s/argocd/argocd-app/stateless/sealed-secrets/generate-random-tls-secret.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Just for quick testing
+
+openssl genrsa -out sealed-secrets.key 4096
+openssl req -new -x509 -key sealed-secrets.key -out sealed-secrets.crt -days 36500 -subj "/CN=sealed-secret-controller"
+# Create the TLS secret (assuming sealed-secrets runs in kube-system namespace)
+kubectl create secret tls sealed-secrets-key \
+  --cert=sealed-secrets.crt \
+  --key=sealed-secrets.key \
+  -n sealed-secrets
