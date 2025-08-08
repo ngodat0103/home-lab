750ef15747232b18a5338a8f39c2171fcd7a86d9
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2025-08-08 15:15:04 +0700

    optimize(tf,cloudflare): modulize personal-firewall

--- tf/cloudflare/main.tf
+++ tf/cloudflare/main.tf
@@ -15,12 +15,12 @@ locals {
   share_comment        = "Managed by Terraform"
   account_id           = "4c8ad4e9fa8213af3fd284bb97b68b5e"
 }
-module "ddns_records"{
-  source = "git::https://github.com/ngodat0103/terraform-module.git//cloudflare/dns-records"
+module "ddns_records" {
+  source               = "git::https://github.com/ngodat0103/terraform-module.git//cloudflare/dns-records?ref=0.0.1"
   cloudflare_api_token = local.cloudflare_api_token
-  zone_id = local.zone_id
-  share_comment = local.share_comment
-  ddns_content = var.ddns_content
+  zone_id              = local.zone_id
+  share_comment        = local.share_comment
+  ddns_content         = var.ddns_content
   dns_records = {
     nextcloud = {
       type = "CNAME"
@@ -34,9 +34,6 @@ module "ddns_records"{
   }
 }
 
-
-
-
 ## Firewall
 resource "null_resource" "download_uptimerobot_ips" {
   provisioner "local-exec" {
@@ -48,28 +45,16 @@ resource "null_resource" "download_uptimerobot_ips" {
 }
 
 
-#Need to import this resource from managed by cloudflare first
-# Example: 
-# terraform import cloudflare_ruleset.default zones/ab6606e8b3aad0b66008eb26f2dd3660/cfe2f8797a314e5ba48192a7e7520bd0
-resource "cloudflare_ruleset" "default" {
-  depends_on = [null_resource.download_uptimerobot_ips]
-  kind       = "zone"
-  name       = "default"
-  phase      = "http_request_firewall_custom"
-  zone_id    = local.zone_id
-  rules = [
-    {
-      description = "Disable log request for gitlab-runenr"
-      expression  = "(http.request.uri.path eq \"/api/v4/jobs/request\" and http.request.method eq \"POST\" and http.user_agent eq \"gitlab-runner 17.8.1 (17-8-stable; go1.23.2 X:cacheprog; linux/amd64)\" and ip.src eq 42.116.6.46)"
-      enable      = true
-      action      = "skip",
-      action_parameters = {
-        ruleset = "current"
-      }
-      logging = {
-        enabled = false
-      }
-    },
+import {
+  to = module.personal_firewall.cloudflare_ruleset.default
+  id = "zones/${local.zone_id}/1036fedd818d45d78dbad2f5dcf4cb17"
+}
+
+module "personal_firewall" {
+  source               = "git::https://github.com/ngodat0103/terraform-module.git//cloudflare/personal-firewall?ref=0.0.1"
+  cloudflare_api_token = local.cloudflare_api_token
+  zone_id              = local.zone_id
+  firewall_rules = [
     {
       description = "Allow healthcheck from UptimeRobot IP addresses"
       expression  = "(http.request.method eq \"HEAD\" and ip.src in {${file("${path.root}/uptimerobot-ips.txt")}})"
