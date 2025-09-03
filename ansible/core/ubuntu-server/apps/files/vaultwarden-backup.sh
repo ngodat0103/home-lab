a9200e7b8025825507169f6b249f08bafa5e2951
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2025-09-03 12:10:03 +0700

    config(ansible,ubuntu-server,vaultwarden): Add script backup, test ok

--- ansible/core/ubuntu-server/apps/files/vaultwarden-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~
-pg_dump > data-backup.sql
-tar -cf vaultwarden-backup-latest.tar .
-mv vaultwarden-backup-latest.tar /mnt/cloud/
-chmod 600 /mnt/recovery/cloud/vaultwarden-backup-latest.tar
