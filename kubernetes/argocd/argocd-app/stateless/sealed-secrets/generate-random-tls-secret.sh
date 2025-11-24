9b87d6871bbe453447203e6bfe49717810bd2c3d
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2025-11-24 23:23:02 +0700

    config(argocd,metallb): Add config

--- kubernetes/argocd/argocd-app/stateless/sealed-secrets/generate-random-tls-secret.sh
+++ kubernetes/argocd/argocd-app/stateless/sealed-secrets/generate-random-tls-secret.sh
@@ -3,7 +3,7 @@
 
 openssl genrsa -out sealed-secrets.key 4096
 openssl req -new -x509 -key sealed-secrets.key -out sealed-secrets.crt -days 36500 -subj "/CN=sealed-secret-controller"
-# Create the TLS secret (assuming sealed-secrets runs in kube-system namespace)
+# Create the TLS secret
 kubectl create secret tls sealed-secrets-key \
   --cert=sealed-secrets.crt \
   --key=sealed-secrets.key \
